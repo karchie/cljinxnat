a75553e4c3f1174e13bb8bd4a7f0f11dfdef1de6
Kevin A. Archie <karchie@wustl.edu>
2013-02-04 15:09:51 -0600

    added functions to search, security

--- src/xnat/search.clj
+++ src/xnat/search.clj
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2012 Washington University
+;;; Copyright (c) 2012,2013 Washington University School of Medicine
 (ns #^{:doc "XNAT search operations"
        :author "Kevin A. Archie <karchie@wustl.edu>"}
   xnat.search
@@ -8,12 +8,24 @@
            (org.nrg.xdat.security XDATUser)))
 
 
-(defn get-stored-search [search-id user]
+(defn get-stored-search
   "Retrieve the named stored search for the provided user."
+  [search-id user]
   (XdatStoredSearch/getXdatStoredSearchsById search-id user true))
 
-(defn make-display-search [& {:keys [user root fields]}]
+(defn get-stored-searches
+  "Retrieve all stored searches for the provided user."
+  [user]
+  (XdatStoredSearch/getAllXdatStoredSearchs user true))
+
+(defn get-field-ids
+  "Get the field IDs for the provided search."
+  [search]
+  (map (memfn getFieldId) (.getSearchField search)))
+
+(defn make-display-search
   "Create a new DisplaySearch."
+  [& {:keys [user root fields]}]
   (let [ds (DisplaySearch.)]
     (when user
       (.setUser ds (if (instance? XDATUser user)
@@ -25,3 +37,4 @@
       (doseq [[elem field] fields]
         (.addDisplayField ds elem field)))
     ds))
+
--- src/xnat/security.clj
+++ src/xnat/security.clj
@@ -1,4 +1,4 @@
-;;; Copyright (c) 2012 Washington University
+;;; Copyright (c) 2012,2013 Washington University School of Medicine
 (ns #^{:doc "XNAT security operations"
        :author "Kevin A. Archie <karchie@wustl.edu>"}
   xnat.security
@@ -7,5 +7,31 @@
 (defn user [u]
   (if (instance? XDATUser u) u (XDATUser. u)))
 
+(def context-service (org.nrg.xdat.XDAT/getContextService))
 
+(def session-registry
+  (.getBean context-service "sessionRegistry"
+            org.springframework.security.core.session.SessionRegistryImpl))
 
+(defn get-sessions
+  "Gets the Spring SessionInformation object for each active session.
+These are a little opaque but contain both the jsessionid and the
+XDATUser."
+  []
+  (mapcat #(.getAllSessions session-registry % false)
+          (.getAllPrincipals session-registry)))
+
+(defn get-session-ids
+  "Get the JSESSIONID strings for every active session."
+  []
+  (map (memfn getSessionId) (get-sessions)))
+
+(defn get-active-users
+  "Get XDATUser objects for all currently logged in users."
+  []
+  (map (memfn getPrincipal) (get-sessions)))
+
+(defn get-active-logins []
+  "Get usernames for all currently logged in users."
+  []
+  (map (memfn getLogin) (get-active-users)))
